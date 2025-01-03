e93a9f1a678236dc5dde2533dba5859ec52951f5
MForofontov <mykyta.forofontov@hotmail.com>
2025-01-03 15:47:36 +0000

    modified script

--- utils/docker-utils/docker-info.sh
+++ utils/docker-utils/docker-info.sh
@@ -2,6 +2,15 @@
 # docker-info.sh
 # Script to show detailed information about Docker containers, images, volumes, and networks
 
+# Check if an output file is provided
+if [ "$#" -eq 1 ]; then
+    OUTPUT_FILE="$1"
+    exec > "$OUTPUT_FILE" 2>&1
+    echo "Writing Docker information to $OUTPUT_FILE"
+else
+    OUTPUT_FILE=""
+fi
+
 echo "Docker Containers:"
 docker ps -a
 echo
@@ -29,3 +38,9 @@ echo
 echo "Docker Info:"
 docker info
 echo
+
+if [ -n "$OUTPUT_FILE" ]; then
+    echo "Docker information has been written to $OUTPUT_FILE"
+else
+    echo "Docker information displayed on the console"
+fi
