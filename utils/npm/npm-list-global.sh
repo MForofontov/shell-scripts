1ece6159dbbb50ac7aaf720354aa5789c001b5f3
MForofontov <mykyta.forofontov@hotmail.com>
2025-01-20 22:55:40 +0000

    feat: Added new scipts

---
+++ utils/npm/npm-list-global.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# npm-list-global.sh
+# Script to list all globally installed NPM packages
+
+# List globally installed NPM packages
+echo "Listing all globally installed NPM packages..."
+npm list -g --depth=0
