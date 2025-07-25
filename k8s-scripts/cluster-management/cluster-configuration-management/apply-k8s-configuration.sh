d5a11d15bf91c10b6659daead5bce65e1321eb39
Mykyta Forofontov <81178208+MForofontov@users.noreply.github.com>
2025-07-25 17:05:00 +0100

    Add shellcheck annotation for common init

--- k8s-scripts/cluster-management/cluster-configuration-management/apply-k8s-configuration.sh
+++ k8s-scripts/cluster-management/cluster-configuration-management/apply-k8s-configuration.sh
@@ -10,6 +10,7 @@ set -euo pipefail
 #=====================================================================
 # CONFIGURATION AND DEPENDENCIES
 #=====================================================================
+# shellcheck source=functions/common-init.sh
 source "$(dirname "$0")/../../../functions/common-init.sh"
 # DEFAULT VALUES
 #=====================================================================
